69d8a497380371ee02041db5708d2cfc678c021d
Brandon Chan <bwchan@utexas.edu>
2021-04-22 10:38:06 -0500

    albums update

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+$(document).ready( function () {
+  $('#table_id').DataTable();
+} );
+
 // populate copyright in footer with current year
 const copyrightYear = document.querySelector('.copyright-year');
 const now = new Date();
