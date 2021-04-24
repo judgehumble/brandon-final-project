8c2c5eb45d2efc7e3a4bb0be8acb41bc091c1c9a
Brandon Chan <bwchan@utexas.edu>
2021-04-24 01:08:07 -0500

    billboard

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-$(document).ready( function () {
-  $('#table_id').DataTable();
-} );
 
 // populate copyright in footer with current year
 const copyrightYear = document.querySelector('.copyright-year');
